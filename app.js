62aeba94d21c1d499f79e9749ef55204e63591d9
Ladylex <116450914+MulLadylex@users.noreply.github.com>
2023-12-08 20:29:29 +0800

    Update app.js
    修复了因异步导致的文件无法上传的问题

--- app.js
+++ app.js
@@ -18,44 +18,51 @@ app.get("/", (req, res) => {
 app.post('/upload', async (req, res) => {
     const title = req.body.title;
     const description = req.body.description;
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({ message: 'No files were uploaded.' });
+    }
     
     const file = req.files.file;
     const filename = file.name;
-    const filepath = 'IPFS/' + filename;
-    await file.mv(filepath,  (err) => {
+    const filepath = 'files/' + filename;
+    file.mv(filepath, async (err) => {
         if (err) {
             console.log(err);
             res.status(500).send("Error Occured");
         }
+        
+        // Upload file to IPFS, get CID
+        const result = await uploadFileToIPFS(filepath);
+        const cid = result.cid.toString();
+        
+        // get metadata
+        const metadata = {
+            title: title,
+            description: description,
+            image: process.env.IMAGE_URL + cid    //改为实际上传的地址
+        };
+        
+        // Upload metadata to IPFS, get CID
+        const metadataResult = await uploadJSONToIPFS(metadata);
+        const metadataCID = metadataResult.cid.toString();
+        
+        // Mint NFT to user (address, metadataurl)
+        await mint(process.env.account_address, process.env.IPFS_URL + metadataCID); 
+        
+        // return data
+        res.json({
+            title: title,
+            description: description,
+            cid: metadataResult.cid.toString(),
+            status: "Uploaded Successfully"
+        });
     });
-
-    // Upload file to IPFS, get CID
-    const result = await uploadFileToIPFS(filepath);
-    const cid = result.cid.toString();
-    
-    // get metadata
-    const metadata = {
-        title: title,
-        description: description,
-        image: process.env.IMAGE_URL + cid    //改为实际上传的地址
-    };
-
-    // Upload metadata to IPFS, get CID
-    const metadataResult = await uploadJSONToIPFS(metadata);
-    const metadataCID = metadataResult.cid.toString();
-    
-    // Mint NFT to user (address, metadataurl)
-    await mint(process.env.account_address, process.env.IPFS_URL + metadataCID); 
-
-    // return data
-    res.json = {
-        title: title,
-        description: description,
-        cid: metadataResult.cid,
-        status: "Uploaded Successfully"
-    };
 });
 
-app.listen(3000, () => {
+const ho = process.env.HOST || "localhost";    //改为实际的host
+const pr = process.env.PORT || 3000;    //改为实际的端口
+
+
+app.listen(pr, ho, () => {
     console.log("Server running on port 3000");
 });
